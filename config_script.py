7a74d701bb69dde78a19e4028c36f8ad501a73fc
Vyacheslav <Vyacheslav@Vyacheslav.com>
2025-09-04 18:27:08 +0400

    update

--- config_script.py
+++ config_script.py
@@ -1,7 +1,7 @@
 import os
-minio_endpoint=""
-temp_access_key=""
-temp_secret_key=""
-bucket_name=""
-epoch=2
-batch_size=64
+minio_endpoint=os.environ.get('minio_endpoint')
+temp_access_key=os.environ.get('minio_access_key')
+temp_secret_key=os.environ.get('minio_secret_key')
+bucket_name=os.environ.get('minio_bucket_name')
+epoch=os.environ.get('epoch', 2)
+batch_size=os.environ.get('minio_batch_size', 64)
